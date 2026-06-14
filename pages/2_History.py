e81a54903be477ad31a8d19a5edf81b588b851db
Timmytech220 <oluwatimileyindaniel4@gmail.com>
2026-06-14 14:39:10 +0100

    2_History.py

--- pages/2_History.py
+++ pages/2_History.py
@@ -1,1 +1,9 @@
+import streamlit as st
+import pandas as pd
 
+st.title("📋 Prediction History")
+if len(st.session_state.history) > 0:
+    st.dataframe(pd.DataFrame(st.session_state.history), use_container_width=True)
+else:
+    st.info("No records found.")
+  
