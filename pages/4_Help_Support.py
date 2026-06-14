1a348c58fe038d5fc8feeb3d9b029ab794b932a0
Timmytech220 <oluwatimileyindaniel4@gmail.com>
2026-06-14 15:41:29 +0100

    4_Help_Support.py

--- pages/4_Help_Support.py
+++ pages/4_Help_Support.py
@@ -1,11 +1,12 @@
 import streamlit as st
+import sys
+import os
 
-import streamlit as st
-from utils import * # Import your functions here
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 if "logged_in" not in st.session_state or not st.session_state.logged_in:
     st.switch_page("app.py")
 
-st.title("💬 Help & Support Center")
-st.write("Developed by: **Ajayi Oluwatimileyin Daniel**")
+st.title("🆘 Help & Support Center")
+st.write("Developed by: **Ajay Oluwatimileyin Daniel**")
 st.write("📞 **WhatsApp:** 09168090334")
-
