ec5455c2ac3c34c16dee09063112e84bec920a59
Timmytech220 <oluwatimileyindaniel4@gmail.com>
2026-06-14 15:17:48 +0100

    2_History.py

--- pages/2_History.py
+++ pages/2_History.py
@@ -1,6 +1,12 @@
 import streamlit as st
 import pandas as pd
 
+import streamlit as st
+from utils import * # Import your functions here
+if "logged_in" not in st.session_state or not st.session_state.logged_in:
+    st.switch_page("app.py")
+
+
 st.title("📋 Prediction History")
 if len(st.session_state.history) > 0:
     st.dataframe(pd.DataFrame(st.session_state.history), use_container_width=True)
