4c8522fa806add398419ca647d88583e4484ddd5
Timmytech220 <oluwatimileyindaniel4@gmail.com>
2026-06-14 15:40:11 +0100

    3_Admin_Dashboard.py

--- pages/3_Admin_Dashboard.py
+++ pages/3_Admin_Dashboard.py
@@ -1,9 +1,11 @@
 import streamlit as st
-import streamlit as st
-from utils import * # Import your functions here
+import sys
+import os
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 if "logged_in" not in st.session_state or not st.session_state.logged_in:
     st.switch_page("app.py")
 
 st.title("⚙️ Admin Dashboard")
 st.write("Bulk upload and system settings will go here.")
-
